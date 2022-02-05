dc744544008c0f9ab1a941c00fb36d8040e17b30
Isaac Lyons <isaac@snowskeleton.net>
2022-02-04 20:30:00 -0500

    Removed unused file

--- nobility/utils.py
+++
@@ -1,7 +0,0 @@
-from .longLists import parts
-
-# Usage: var = fetchPartsFor('Dell 3100 (Touch, +USB)')
-def fetchPartsFor(model):
-    for (key, value) in parts.items():
-        if key == model:
-            return value

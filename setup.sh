e82b9f0dc57bbd35275798e10f97f69928b59c7a
Isaac Lyons <isaac@snowskeleton.net>
2022-02-06 21:09:21 -0500

    All the fiddly details to move to postgres

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 # prelim
 sudo apt update
 sudo apt-get install -y python3 python3-pip python3-venv python3-tk apache2 libapache2-mod-wsgi-py3 sqlite3
-sudo python3 -m venv ./venv
-alias python3="venv/bin/python3"
+sudo apt-get install libpq-dev postgresql postgresql-contrib
+python3 -m venv ./venv
+alias python="venv/bin/python3"
 source venv/bin/activate
-sudo venv/bin/python3 -m pip install -r requirements.txt
+sudo python -m pip install -r requirements.txt
 
 # .env config
 key=$(openssl rand -base64 32)
@@ -18,19 +19,21 @@ echo "MEDIA_URL='media/'" >> .env
 echo "MEDIA_ROOT='media/'" >> .env
 
 # site setup
-sudo python3 manage.py collectstatic
-sudo sed "s/changeMe/${USER}/g" default_setup/defaultApache > tmp
+python manage.py collectstatic
+sed "s/changeMe/${USER}/g" default_setup/defaultApache > tmp
 sudo cp tmp /etc/apache2/sites-available/royal.conf
-sudo a2ensite royal.conf
-sudo a2dissite 000-default.conf
+sudo a2ensite royal.conf && sudo a2dissite 000-default.conf
 
 # permissions and db
-sudo rm db.sqlite3
-sudo touch db.sqlite3 && mkdir media
-sudo chown www-data:www-data db.sqlite3
-sudo python3 manage.py migrate --run-syncdb
-sudo python3 manage.py makemigrations
-sudo python3 manage.py migrate
+# create user king;
+# create database treasure;
+# alter role king with password 'gold';
+# grant all privileges on database treasure to king;
+# alter database treasure owner to king;
+rm db.sqlite3 && touch db.sqlite3 && mkdir media
+sudo chown -R www-data:www-data ../nobility
+python3 manage.py migrate --run-syncdb
+python3 manage.py makemigrations && python3 manage.py migrate
 sudo chown -R www-data:www-data ../nobility
 sudo chmod -R 775 media
 sudo chmod 664 db.sqlite3

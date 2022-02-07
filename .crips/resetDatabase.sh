815c360fadf9f2caee0dc3f3decda7ec4bbf7a4e
Isaac Lyons <isaac@snowskeleton.net>
2022-02-06 21:14:35 -0500

    More utility updates

--- .crips/resetDatabase.sh
+++ .crips/resetDatabase.sh
@@ -2,5 +2,6 @@ rm db.sqlite3
 python manage.py migrate --run-syncdb
 python manage.py makemigrations
 python manage.py migrate
+printf "Username: "
 read username
 source .crips/createSuperUser.sh ${username}

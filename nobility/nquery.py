f7ac5ee6faf93727d6eda753e0b88cecfffd61cb
Isaac Lyons <isaac@snowskeleton.net>
2022-02-06 19:46:37 -0500

    Duplicated tickets() method for parts

--- nobility/nquery.py
+++ nobility/nquery.py
@@ -1,15 +1,17 @@
-from .models import Ticket
+from .models import Ticket, Part
 from django.db.models import Q
 
 class NQuery():
 
-    # accepts HTTP request. returns QuerySet object filtered by params in request if possible, otherwise returns all Ticket()s.
+    # accepts HTTP request.
+    # returns QuerySet object filtered by params in request if possible,
+    ##otherwise returns all Ticket()s.
     @classmethod
     def tickets(self, request):
         try:
             get = request.GET
         except:
-            print('error: NQeuery.tickets() failed to find request.GET. returned all() instead')
+            print('error: NQeuery.tickets() failed to parse request.GET. returned all() instead')
             return Ticket.objects.all()
 
         query = get['q']
@@ -37,3 +39,35 @@ class NQuery():
                 field[0] = False
 
         return Ticket.objects.filter(queries)
+
+    @classmethod
+    def parts(self, request):
+        try:
+            get = request.GET
+        except:
+            print('error: NQeuery.parts() failed to parse request.GET. returned all() instead')
+            return Part.objects.all()
+
+        query = get['q']
+        queries = (( # effectly a 'full-text' search, but sqlite is special
+            Q(name__icontains=query) |
+            Q(mpn__icontains=query) |
+            Q(sku__icontains=query))
+        )
+
+        fields = [ 
+            [None, 'name', ''],
+            [None, 'mpn', ''],
+            [None, 'sku', ''],
+            [None, 'ordered', ''],
+            [None, 'replaced', ''],
+        ]
+        for field in fields:
+            if f'{field[1]}' in get.getlist('toggle'):
+                field[0] = True
+                field[2] = f'{get[field[1]]}'
+                queries = queries & Q(**({f'{field[1]}'+'__icontains': f'{field[2]}'}))
+            else:
+                field[0] = False
+
+        return Part.objects.filter(queries)

7488ffe82aa4984806b63518bc5f58259dc03a1d
Isaac Lyons <isaac@snowskeleton.net>
2022-02-08 16:32:02 -0500

    Changed setup file

--- setup.sh
+++ setup.sh
@@ -3,11 +3,12 @@ sudo apt update
 sudo apt-get install -y python3 python3-pip python3-venv python3-tk apache2 libapache2-mod-wsgi-py3 sqlite3
 sudo apt-get install libpq-dev postgresql postgresql-contrib
 python3 -m venv ./venv
-alias python="venv/bin/python3"
 source venv/bin/activate
+alias python="venv/bin/python3"
 sudo python -m pip install -r requirements.txt
 
 # .env config
+sudo rm -f .env
 key=$(openssl rand -base64 32)
 echo "SECRET_KEY=${key}" > .env
 printf "hostname (name of your website, or IP address): "
@@ -19,8 +20,8 @@ echo "MEDIA_URL='media/'" >> .env
 echo "MEDIA_ROOT='media/'" >> .env
 
 # site setup
-python manage.py collectstatic
-sed "s/changeMe/${USER}/g" default_setup/defaultApache > tmp
+sudo python manage.py collectstatic
+sudo sed "s/changeMe/${USER}/g" default_setup/defaultApache > tmp
 sudo cp tmp /etc/apache2/sites-available/royal.conf
 sudo a2ensite royal.conf && sudo a2dissite 000-default.conf
 
@@ -30,10 +31,10 @@ sudo a2ensite royal.conf && sudo a2dissite 000-default.conf
 # alter role king with password 'gold';
 # grant all privileges on database treasure to king;
 # alter database treasure owner to king;
-rm db.sqlite3 && touch db.sqlite3 && mkdir media
+sudo rm db.sqlite3 && touch db.sqlite3 && mkdir media
 sudo chown -R www-data:www-data ../nobility
-python3 manage.py migrate --run-syncdb
-python3 manage.py makemigrations && python3 manage.py migrate
+sudo python3 manage.py migrate --run-syncdb
+sudo python3 manage.py makemigrations && python3 manage.py migrate
 sudo chown -R www-data:www-data ../nobility
 sudo chmod -R 775 media
 sudo chmod 664 db.sqlite3

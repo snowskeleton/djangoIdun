a5d0a98a5b267fa70da38ad2175df94c462a943c
Isaac Lyons <isaac@snowskeleton.net>
2022-02-08 15:52:50 -0500

    Add a log when ticket is created

--- nobility/views.py
+++ nobility/views.py
@@ -137,6 +137,11 @@ def addTicket(request):
         ticket = form.save(commit=False)
         ticket.serial = (ticket.serial).upper()
         ticket.save()
+        Note.log(
+            ticket,
+            "created",
+            user=request.user
+        )
         return redirect(f"/ticket/{ticket.id}")
 
     return render(request, "nobility/addTicket.html", {"form": form})
